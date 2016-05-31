8403f899e8c7ab1cea243e0e7b0332474d2a646d
Richard Stelling <richard.stelling@naimaudio.com>
2016-05-31 14:51:50 +0100

    Created 'dummy' Networking framework to allow Swift to import the required networking C APIs.

---
+++ Networking/Networking.h
@@ -0,0 +1,19 @@
+//
+//  Networking.h
+//  Networking
+//
+//  Created by Richard Stelling on 30/05/2016.
+//  Copyright © 2016 Richard Stelling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Networking.
+FOUNDATION_EXPORT double NetworkingVersionNumber;
+
+//! Project version string for Networking.
+FOUNDATION_EXPORT const unsigned char NetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Networking/PublicHeader.h>
+
+
