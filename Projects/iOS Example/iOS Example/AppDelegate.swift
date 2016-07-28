2843bd3ffc85a346e28f824eddcf66b18d4b1487
Richard Stelling <richard.stelling@naimaudio.com>
2016-07-28 15:09:19 +0100

    🗑Tidy.

--- Projects/iOS Example/iOS Example/AppDelegate.swift
+++ Projects/iOS Example/iOS Example/AppDelegate.swift
@@ -46,4 +46,3 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 }
-
