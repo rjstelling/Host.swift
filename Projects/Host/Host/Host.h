cfca8dd3c9e19a914feb9e85773a8c4a50a8bc09
Richard Stelling <richard.stelling@naimaudio.com>
2016-08-08 10:49:59 +0100

    Fixed compile issues.

--- Projects/Host/Host/Host.h
+++ Projects/Host/Host/Host.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Richard Stelling. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Host.
 FOUNDATION_EXPORT double HostVersionNumber;
